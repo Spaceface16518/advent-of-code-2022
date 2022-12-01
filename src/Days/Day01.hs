a4e3115bb4576bfeedfeaf4352d93e69606c09aa
Spaceface16518 <amrit.rathie@gmail.com>
2022-12-01 05:33:21 -0600

    solution day 1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -1,39 +1,37 @@
 module Days.Day01 (runDay) where
 
-{- ORMOLU_DISABLE -}
+import Data.Attoparsec.Text hiding (take)
 import Data.List
 import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.Map.Strict qualified as Map
 import Data.Maybe
 import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.Set qualified as Set
 import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Vector qualified as Vec
 import Data.Void
-{- ORMOLU_ENABLE -}
+import Debug.Trace (trace)
+import Program.RunDay qualified as R (Day, runDay)
+import Util.Util qualified as U
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` char '\n' `sepBy` string "\n\n"
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Int]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = foldr (max . sum) 0
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . take 3 . sortBy (flip compare) . map sum
