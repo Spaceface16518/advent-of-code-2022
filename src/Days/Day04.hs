b3042ae34c0d84594755aa228de587c06d2cc5ff
Spaceface16518 <amrit.rathie@gmail.com>
2022-12-04 00:28:58 -0600

    day 4 part 1

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -14,6 +14,7 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Control.Monad
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +22,44 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many' (pair <* endOfLine)
+  where
+    range = do
+      l <- decimal
+      char '-'
+      r <- decimal
+      return (l, r)
+    pair = do
+      l <- range
+      char ','
+      r <- range
+      return (l, r)
 
 ------------ TYPES ------------
-type Input = Void
+type Range = (Int, Int)
 
-type OutputA = Void
+type Pair = (Range, Range)
 
-type OutputB = Void
+type Input = [Pair]
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+contains :: Range -> Range -> Bool
+contains (l, r) (x, y) = l <= x && y <= r
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = length . filter (ap ((||) . uncurry contains) (uncurry . flip $ contains))
 
 ------------ PART B ------------
+
+overlaps :: Pair -> Bool
+overlaps ((l, r), (x, y)) = l <= y && x <= r
+
+overlap :: Pair -> Int
+overlap ((l, r), (x, y)) = max 0 $ min r y - max l x
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . map overlap . filter overlaps
